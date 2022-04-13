c964cd4ad2e90f311ac57bdcd7669b12d38d522a
RainhaMFF <93388051+RainhaMFF@users.noreply.github.com>
2022-04-13 10:59:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+var floresta
+var LoloS2
+var Lolos2
+function preload(){ // função que carregar todas as imagens e animações
+  
+floresta = loadImage("bg.png");
+Lolos2 = loadImage("lolo<3.png");
+
+}
+
+function setup(){ // todas as configuraçoes dos objetos
+  createCanvas(1669,912);
+  LoloS2 = createSprite(479,722,20,20)
+  LoloS2.addImage(Lolos2);
+LoloS2.scale = 0.3
+}
+
+function draw(){
+  background(floresta);
+  drawSprites(); 
+
+  text (mouseX+","+mouseY,mouseX,mouseY);
+  andei();
+
+}
+function andei(){
+  if(keyDown("up")){
+LoloS2.y -= 3;
+  }
+  if(keyDown("down")){
+LoloS2.y += 3;
+  }
+if(keyDown("left")){
+LoloS2.x -=3;
+}
+if(keyDown("right")){
+LoloS2.x += 3;
+}
+}
+
