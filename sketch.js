1469a908e6af4b1c176d3694c85d4c3307b0f0ca
RainhaMFF <93388051+RainhaMFF@users.noreply.github.com>
2022-04-20 15:58:49 -0700

    aula46

--- sketch.js
+++ sketch.js
@@ -2,9 +2,13 @@
 var floresta
 var LoloS2
 var Lolos2
+var Algoritimo5, Algoritimo5jr
+var Lily, LilyAxolotl
 function preload(){ // função que carregar todas as imagens e animações
   floresta = loadImage("bg.png");
   Lolos2 = loadImage("lolo_3.png");
+  Algoritimo5 = loadImage("algt5.png");  
+  Lily = loadAnimation("ax7.png","ax8.png");
 }
 
 
@@ -13,6 +17,15 @@ function setup(){ // todas as configuraçoes dos objetos
   LoloS2 = createSprite(479,722,20,20)
   LoloS2.addImage(Lolos2);
   LoloS2.scale = 0.3
+
+Algoritimo5jr = createSprite(613,609,20,20);
+Algoritimo5jr.addImage(Algoritimo5);  
+Algoritimo5jr.scale = 0.2;
+
+Lily.frameDelay = 11;
+LilyAxolotl = createSprite(1293,313,20,20);
+LilyAxolotl.addAnimation("Lily",Lily);
+LilyAxolotl.scale = 0.4
 }
 
 function draw(){
